301ff2acf52ea3ff88a79515cd0373123299db23
EvgeniyPusser <evggeniipusser88@gmail.com>
2026-01-08 00:49:45 +0200

    address and some spreads

--- persons.js
+++ persons.js
@@ -9,46 +9,109 @@ const result = document.getElementById('result');
 const stats = document.getElementById('stats');
 
 addPerson.onclick = function () {
-
-    if (persons.findIndex(person => person.id === +id.value) < 0) {
-
+    if (persons.findIndex(person => person.id === id) < 0) {
         const person = new Person(
             id.value,
             firstName.value,
             lastName.value,
             new Date(birthday.value) // FIX
         );
-
         persons.push(person);
+        // const addressFields = createFieldsFromObject(person.address);
+        // document.body.append(addressFields.container);
+        // const saveAddressBtn = createButton(
+        //     'Save address',
+        //     () => {
+        //         Object.keys(addressFields.fields).forEach(key => {
+        //             person.address[key] = addressFields.fields[key].value;
+        //         });
+        //         console.log('Saved address:', person.address);
+        //     }
+        // );
 
         const li = document.createElement('li');
+        const buttonDel = createButton(
+            'Delete',
+            (e, array, person, showStats, message) => {
+                e.target.parentElement.remove();
+                removeFromArrayById(array, person.id);
+                showStats();
+                alert(message + person.firstName);
+            },
+            persons,                 // array
+            person,                  // person
+            showStats,               // функция
+            'Удалён пользователь: '  // строка
+        );
 
-        const buttonDel = createButtonDel(() => {
-            const index = persons.findIndex(person => person.id === +id.value);
-            persons.splice(index, 1);
-            showStats();
-        });
+        // const buttonAddress = createButton(
+        //     'Address',
+        //     (e, person) => {
+        //         if (!person.address) {
+        //             person.address = new Address('', '', '', '', '', '', '');
+        //         }
+        //         const { container, fields } = createFieldsFromObject(person.address);
+        //         const saveBtn = createButton('Save', () => {
+        //             Object.keys(fields).forEach(key => {
+        //                 person.address[key] = fields[key].value;
+        //             });
+        //             container.remove();
+        //         });
+        //         document.body.append(container, saveBtn);
+        //     },
+        //     person
+        // );
+        //
+        // const buttonAddress = createButton(
+        //     'Address',
+        //     (e, person) => {
+        //         if (!person.address) {
+        //             person.address = new Address('', '', '', '', '', '', '');
+        //         }
+        //
+        //         const { table, saveBtn } = renderObjectAsTable(
+        //             person.address,
+        //             (updatedAddress) => {
+        //                 Object.assign(person.address, updatedAddress);
+        //             },
+        //             addressLabels // ← ЛЕЙБЛЫ
+        //         );
+        //
+        //         document.body.append(table, saveBtn);
+        //     },
+        //     person
+        // );
 
-        li.append(person.toString(), buttonDel);
+        const buttonAddress = createButton(
+            'Address',
+            (e, person) => {
+                showAddressModal(person);
+            },
+            person
+        );
+
+
+
+
+
+        // li.append(person.toString(), buttonDel);
+        li.append(person.toString(), buttonDel, buttonAddress);
+
         result.appendChild(li);
+        document.body.append(saveAddressBtn);
         showStats();
-
     } else {
         alert(`Person with ID = ${id.value} exists`); // FIX
     }
-
     id.value = firstName.value = lastName.value = birthday.value = ''; // FIX
 };
 
+
 function showStats() {
-
     const divStats = document.createElement('div');
-    divStats.className = 'stats-box'; // ← ВАЖНО
-
+    divStats.className = 'stats-box';
     if (persons.length) {
-
         const ages = persons.map(p => getAge(p.birthday));
-
         const minY = Math.min(...ages);
         const maxY = Math.max(...ages);
         const averY = Math.round(
@@ -62,7 +125,6 @@ function showStats() {
             createInfoElement(`Average age: ${averY}`, 'p')
         );
     }
-
     if (stats.children.length > 1) {
         stats.replaceChild(divStats, stats.lastChild);
     } else {
@@ -72,12 +134,35 @@ function showStats() {
 
 
 function Person(id, firstName, lastName, birthday) {
-    this.id = +id;
+    this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.birthday = birthday;
-
     this.toString = function () {
         return `ID: ${this.id}; First Name: ${this.firstName}; Last Name: ${this.lastName}; Birthday: ${this.birthday.toISOString().slice(0,10)};`; // FIX
     };
+}
+
+
+
+function removeFromArrayById(array, id) {
+    const index = array.findIndex(item => item.id === id);
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+function Address(index, country, city, street, house, apartment, phone) {
+    this.index = index;
+    this.country = country;
+    this.city = city;
+    this.street = street;
+    this.house = house;
+    this.apartment = apartment;
+    this.phone = phone;
+    this.toString = function () {
+        return `Index: ${this.index}; Country: ${this.country}; City: ${this.city}; Street: ${this.street} House: ${this.house}; Apartment: ${this.apartment}; Phone: ${this.phone}`;
+    };
+
+
 }
--- utils.js
+++ utils.js
@@ -1,15 +1,17 @@
-function createButtonDel(callBack) {
-  const buttonDelete = document.createElement('button');
-  buttonDelete.append('Delete');
-buttonDelete.addEventListener('click', function (e) {
-    e.target.parentElement.remove();
-    if (typeof callBack === 'function') {
-        callBack();
-    }
-})
-    buttonDelete.className = 'delB';
-    return buttonDelete;
+function createButton(text, callBack, ...args) {
+    const button = document.createElement('button');
+    button.textContent = text;
+    button.className = 'btn';
+
+    button.addEventListener('click', (e) => {
+        if (typeof callBack === 'function') {
+            callBack(e, ...args);
+        }
+    });
+
+    return button;
 }
+
 
 function createInfoElement(content, tag) {
     const element = document.createElement(tag);
@@ -30,3 +32,119 @@ function getAge(birthday) {
     }
     return age;
 }
+
+function renderObjectAsTable(obj, onSave, labels = {}) {
+    const table = document.createElement('table');
+    table.border = '1';
+
+    Object.entries(obj).forEach(([key, value]) => {
+        const tr = document.createElement('tr');
+
+        const tdKey = document.createElement('td');
+        tdKey.textContent = labels[key] ?? key; // ← ВОТ ТУТ ВСЯ МАГИЯ
+
+        const tdValue = document.createElement('td');
+        const input = document.createElement('input');
+        input.value = value ?? '';
+        input.dataset.key = key;
+
+        tdValue.append(input);
+        tr.append(tdKey, tdValue);
+        table.append(tr);
+    });
+
+    const saveBtn = document.createElement('button');
+    saveBtn.textContent = 'Save';
+
+    saveBtn.onclick = () => {
+        const inputs = table.querySelectorAll('input');
+        const result = {};
+
+        inputs.forEach(input => {
+            result[input.dataset.key] = input.value;
+        });
+
+        onSave(result);
+        table.remove();
+        saveBtn.remove();
+    };
+
+    return { table, saveBtn };
+}
+
+
+
+function createField({ label, name, type = 'text', value = '' }) {
+    const wrapper = document.createElement('div');
+
+    const labelEl = document.createElement('label');
+    labelEl.textContent = label + ': ';
+
+    const input = document.createElement('input');
+    input.type = type;
+    input.name = name;
+    input.value = value;
+
+    labelEl.append(input);
+    wrapper.append(labelEl);
+
+    return {
+        element: wrapper,
+        input
+    };
+}
+
+function createFieldsFromObject(obj) {
+    const fields = {};
+    const container = document.createElement('div');
+
+    Object.entries(obj).forEach(([key, value]) => {
+        const field = createField({
+            label: key,
+            name: key,
+            value: value ?? ''
+        });
+
+        fields[key] = field.input;
+        container.append(field.element);
+    });
+
+    return {
+        container,
+        fields
+    };
+}
+
+function showAddressModal(person) {
+    if (!person.address) {
+        person.address = new Address('', '', '', '', '', '', '');
+    }
+
+    const overlay = document.createElement('div');
+    overlay.className = 'modal-overlay';
+
+    const modal = document.createElement('div');
+    modal.className = 'modal';
+
+    const { table, saveBtn } = renderObjectAsTable(
+        person.address,
+        (updatedAddress) => {
+            Object.assign(person.address, updatedAddress);
+            overlay.remove();
+        }
+    );
+
+    modal.append(table, saveBtn);
+    overlay.append(modal);
+
+    // закрытие по клику вне окна
+    overlay.addEventListener('click', (e) => {
+        if (e.target === overlay) {
+            overlay.remove();
+        }
+    });
+
+    document.body.append(overlay);
+}
+
+
